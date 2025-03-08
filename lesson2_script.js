a82165d0bac74c6416dc34d66fba6bd3c115c4b4
Zack <zack.neill2012@gmail.com>
2025-03-08 06:01:44 -0500

    Added Lesson 2: JavaScript Placement with Inline, Internal, and External JS

---
+++ lesson2_script.js
@@ -0,0 +1,4 @@
+// External JavaScript - Changes paragraph text when the button is clicked
+function changeParagraph() {
+    document.getElementById("description").innerText = "The text has been changed using an external JavaScript file!";
+}
