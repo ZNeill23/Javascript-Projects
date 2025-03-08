d958af77b68ab055fe5685ae16c365e09b013597
Zack <zack.neill2012@gmail.com>
2025-03-08 06:04:19 -0500

    Added Lesson 3: JavaScript Output Methods

---
+++ lesson3_script.js
@@ -0,0 +1,19 @@
+// Change innerHTML to update text on the page
+function showInnerHTML() {
+    document.getElementById("output").innerHTML = "This text was changed using innerHTML!";
+}
+
+// Use document.write() (Only for demonstration, not recommended in practice)
+function useDocumentWrite() {
+    document.write("This was written using document.write(). Warning: This will overwrite the page!");
+}
+
+// Show an alert popup
+function useAlert() {
+    alert("This is an alert box using JavaScript!");
+}
+
+// Log a message to the console
+function useConsoleLog() {
+    console.log("This message was logged using console.log()");
+}
